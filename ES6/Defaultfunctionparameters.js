7a55d5735cf2d8ddfbb502ce284f190d26edca43
HimanshuP90 <himanshup895@gmail.com>
2017-12-28 13:44:41 +0530

    function add(x,y) {

---
+++ ES6/Defaultfunctionparameters.js
@@ -0,0 +1,19 @@
+
+// function add(x,y) {
+// 	console.log(x+y);
+// }
+
+// add();
+
+
+// function add(x=6,y=9) {
+// 	console.log(x+y);
+// }
+
+// add();
+
+function havFun(ativityName='hiking', time=3) {
+	console.log(`Today I will go ${ativityName} for ${time} hours.`);
+}
+
+havFun('biking', 5);
