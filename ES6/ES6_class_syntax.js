e2a291536302fb9a4d347c4a6a50e3000f25fdc3
HimanshuP90 <himanshup895@gmail.com>
2017-12-28 19:38:54 +0530

    ES6_class_syntax.js

---
+++ ES6/ES6_class_syntax.js
@@ -0,0 +1,15 @@
+//ES6_class_syntax.js
+
+class Vehicle {
+	constructor(description, wheels) {
+		this.description = description;
+		this.wheels = wheels;
+	}
+	describeYourself() {
+		console.log(`I am a ${this.description} with ${this.wheels} wheels`);		
+	}
+}
+
+var coolSkiVan = new Vehicle("cool ski van", 4);
+
+coolSkiVan.describeYourself();
