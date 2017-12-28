6a36f58a55fc9fd6c52a132c5d518dd9c2ac8c3a
HimanshuP90 <himanshup895@gmail.com>
2017-12-28 17:03:57 +0530

    Building promises

---
+++ ES6/Symbols.js
@@ -0,0 +1,61 @@
+//The building blocks of all of our JavaScript applications are JavaScript primitives. Primitives in JavaScript are types, like numbers, strings, arrays, and objects. Symbols are a new primitive type in ES6. Symbols are often used as unique identifiers because they won't conflict with object keys that are strings. Symbols also help us to create customized iteration behavior
+
+
+// const id = Symbol();
+// const courseInfo = {
+// 	title: "ES6",
+// 	topics:["babel", "syntax", "functions", "classes"]
+// };
+
+// courseInfo[id] = 41284;
+// console.log(courseInfo)
+
+
+// const id = Symbol();
+// const courseInfo = {
+// 	title: "ES6",
+// 	topics:["babel", "syntax", "functions", "classes"]
+// 	,id: "js-course"
+// };
+
+// courseInfo[id] = 41284;
+// console.log(courseInfo)
+
+
+// const delay = seconds => {
+// 	return new Promise(resolve => {
+// 		setTimeout(
+// 			() => resolve(` ${seconds} second delay is up`), 
+// 			seconds*1000)
+// 	});
+// };
+
+// console.log("Zero seconds");
+// delay(2).then(() => console.log("one seconds"));
+// delay(4).then(() => console.log("three seconds"));
+
+
+//Often we use promises to load data. Let's create a function called space people that returns a promise
+const spacePeople = () => {
+	return new Promise((resolve, rejects) => {
+		const api = 'http://api.open-notify.org/astros.json';
+		const request = new XMLHttpRequest();
+		request.open('GET', api);
+		request.onload = () => {
+			if( request.status === 200) {
+				resolve(JSON.parse(request.response));
+			} else {
+				rejects(Error(request.statusText));
+			}
+		};
+		request.onerror = err => rejects(err);
+		request.send();
+	});
+};
+
+spacePeople().then(
+	spaceData => console.log(spaceData),
+	err => console.error(
+		new Error('Cannot load space people')
+	)
+);
