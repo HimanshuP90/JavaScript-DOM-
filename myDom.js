40f1bfc9f9a696aaf964673cc44648e6dd9d3313
HimanshuP90 <himanshup895@gmail.com>
2017-12-31 02:43:09 +0530

    jQuery event added

--- myDom.js
+++ myDom.js
@@ -310,9 +310,12 @@ window.onload = function() {
 	preventEvent();
 }
 
+$("p").click(function(){
+	$(this).text("You clicked me ..!!");
+});
 
 
-/What is the Closure Compiler?
+// /What is the Closure Compiler?
 
 
-The Closure Compiler is a tool for making JavaScript download and run faster. Instead of compiling from a source language to machine code, it compiles from JavaScript to better JavaScript. It parses your JavaScript, analyzes it, removes dead code and rewrites and minimizes what's left. It also checks syntax, variable references, and types, and warns about common JavaScript pitfalls.
+// The Closure Compiler is a tool for making JavaScript download and run faster. Instead of compiling from a source language to machine code, it compiles from JavaScript to better JavaScript. It parses your JavaScript, analyzes it, removes dead code and rewrites and minimizes what's left. It also checks syntax, variable references, and types, and warns about common JavaScript pitfalls.
